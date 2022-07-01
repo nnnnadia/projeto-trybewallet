59fcd84881dd118ea0b8143545ce4ece26cddc2f
nnnnadia <tristaonadia@gmail.com>
2022-07-01 19:17:47 -0300

    Requisito 01: É possível realizar o login na página inicial.

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import actSaveUserEmail from '../actions';
+
+class LoginForm extends Component {
+  state = {
+    email: '',
+    password: '',
+    isLoginButtonDisabled: true,
+  };
+
+  validadeInput = () => {
+    const { email, password } = this.state;
+    const emailValidated = /[\d|\w|\\.]+@[\w]+\.[\w]+/i.test(email);
+    const passwordValidated = /[\d|\w]{6,}/i.test(password);
+    const validated = !emailValidated || !passwordValidated;
+    this.setState({ isLoginButtonDisabled: validated });
+  }
+
+  handleInputChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value }, () => this.validadeInput());
+  }
+
+  handleLogin = (event) => {
+    event.preventDefault();
+    const {
+      state: { email },
+      props: { history, saveUserEmail },
+    } = this;
+    saveUserEmail(email);
+    history.push('/carteira');
+  }
+
+  render() {
+    const {
+      state: {
+        email,
+        password,
+        isLoginButtonDisabled,
+      },
+      handleInputChange,
+      handleLogin,
+    } = this;
+    return (
+      <form
+        onSubmit={ handleLogin }
+      >
+        <input
+          type="email"
+          name="email"
+          value={ email }
+          onChange={ handleInputChange }
+          data-testid="email-input"
+        />
+        <input
+          type="password"
+          name="password"
+          value={ password }
+          onChange={ handleInputChange }
+          data-testid="password-input"
+        />
+        <button
+          type="submit"
+          disabled={ isLoginButtonDisabled }
+        >
+          Entrar
+        </button>
+      </form>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  saveUserEmail: (email) => dispatch(actSaveUserEmail(email)),
+});
+
+LoginForm.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+  saveUserEmail: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(LoginForm);
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import LoginForm from '../components/LoginForm';
 
-class Login extends React.Component {
+class Login extends Component {
   render() {
-    return <div>Login</div>;
+    const { history } = this.props;
+    return (
+      <div>
+        <h2>Login</h2>
+        <LoginForm history={ history } />
+      </div>
+    );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Login;
